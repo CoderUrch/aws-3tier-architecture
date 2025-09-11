853ac26020820dbcd79a6b3e9d17b6bf9eeb57b4
Uche <urch4gold@gmail.com>
2025-09-11 17:03:49 +0100

    changed remote backend to S3

--- infra_setup/backend.tf
+++ infra_setup/backend.tf
@@ -1,10 +1,7 @@
 terraform {
-  cloud {
-
-    organization = "rover"
-
-    workspaces {
-      name = "local"
-    }
+  backend "s3" {
+    bucket = "terraform-state-bucket-unique-name"
+    key    = "3tier/terraform.tfstate"
+    region = "eu-north-1"
   }
 }
